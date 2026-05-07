588a794f4964de723690a4159a8e905f5b703464
ostrowskii <vanessaostroski08@gmail.com>
2026-05-07 17:03:07 -0400

    Rename server rooms to Sala 1-3

--- src/network/config.js
+++ src/network/config.js
@@ -3,18 +3,18 @@ export const VIBINET_SERVER_LABEL = "net.vibistudiotest.site";
 
 export const SERVER_CHOICES = [
   {
-    id: "castelo-rubi",
-    name: "Castelo Rubi",
-    room: "betrayal-castelo-rubi",
+    id: "sala-1",
+    name: "Sala 1",
+    room: "betrayal-sala-1",
   },
   {
-    id: "torre-dourada",
-    name: "Torre Dourada",
-    room: "betrayal-torre-dourada",
+    id: "sala-2",
+    name: "Sala 2",
+    room: "betrayal-sala-2",
   },
   {
-    id: "patio-onix",
-    name: "Patio Onix",
-    room: "betrayal-patio-onix",
+    id: "sala-3",
+    name: "Sala 3",
+    room: "betrayal-sala-3",
   },
 ];
