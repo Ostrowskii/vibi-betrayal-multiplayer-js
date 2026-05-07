1cb046d62d8ae5cc768c3fa5d9a515107c49f764
ostrowskii <vanessaostroski08@gmail.com>
2026-05-07 11:51:05 -0400

    Rotate server room codes again

--- src/network/config.js
+++ src/network/config.js
@@ -3,18 +3,18 @@ export const VIBINET_SERVER_LABEL = "net.vibistudiotest.site";
 
 export const SERVER_CHOICES = [
   {
-    id: "nx7-k4m",
-    name: "NX7-K4M",
-    room: "betrayal-nx7-k4m",
+    id: "m8x-p2q",
+    name: "M8X-P2Q",
+    room: "betrayal-m8x-p2q",
   },
   {
-    id: "q2v-r8t",
-    name: "Q2V-R8T",
-    room: "betrayal-q2v-r8t",
+    id: "h4n-w7z",
+    name: "H4N-W7Z",
+    room: "betrayal-h4n-w7z",
   },
   {
-    id: "z5p-l9c",
-    name: "Z5P-L9C",
-    room: "betrayal-z5p-l9c",
+    id: "t6r-k3v",
+    name: "T6R-K3V",
+    room: "betrayal-t6r-k3v",
   },
 ];
